85fd8f80b278236daaae3dfc9ebb4a8f78374230
sdobrimutrom <gladyshev15081508@gmail.com>
2025-05-15 18:58:04 +0500

    Change password: backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, CurrentUserView, LoggingTokenObtainPairView
+from .views import RegisterView, CurrentUserView, LoggingTokenObtainPairView, change_password
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,4 +11,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/', LoggingTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('change-password/', change_password, name="change_password")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
 import logging
@@ -31,3 +33,20 @@ class LoggingTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class LoggingTokenObtainPairView(TokenObtainPairView):
     serializer_class = LoggingTokenObtainPairSerializer
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    user = request.user
+    old_password = request.data.get("old_password")
+    new_password = request.data.get("new_password")
+
+    if not user.check_password(old_password):
+        return Response({"detail": "Wrong old password"}, status=400)
+    if not new_password or len(new_password) < 6:
+        return Response({"detail": "Password is to short"}, status=400)
+    
+    user.set_password(new_password)
+    user.save()
+
+    return Response({"detail": "Password is changed successfully"}, status=200)
