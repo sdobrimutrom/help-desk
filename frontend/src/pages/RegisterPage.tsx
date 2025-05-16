df08f02e51a0d04723c8cd0ed8f2a867ffe9224a
sdobrimutrom <gladyshev15081508@gmail.com>
2025-05-16 13:37:41 +0500

    Sign up page styles

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { registerUser, loginUser } from "../api/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [error, setError] = useState("");
+    const navigate = useNavigate();
 
     async function handleRegister(e: React.FormEvent) {
         e.preventDefault()
@@ -18,20 +20,33 @@ export default function RegisterPage() {
 
         const token = await loginUser(username, password);
         if (token) {
-            window.location.href = "/me";
+            window.location.href = "/home";
         }
     }
 
     return (
-        <div>
-            <h2>Регистрация</h2>
-            <form onSubmit={handleRegister}>
-                <input placeholder="Логин" value={username} onChange={e => setUsername(e.target.value)} />
-                <input type="password" placeholder="Пароль" value={password} onChange={e => setPassword(e.target.value)} />
-                <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                <button type="submit">Зарегистрироваться</button>
-                {error && <p style={{ color: "red" }}>{error}</p>}
-            </form>
+        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: "100vh"}}>
+            <div className="card p-4" style={{ width: "100%", maxWidth: "400px"}}>
+                <h1 className="text-center mb-3">Registration</h1>
+                <form onSubmit={handleRegister}>
+                    <div className="mb-3">
+                        <label className="form-label">Login</label>
+                        <input className="form-control" value={username} onChange={e => setUsername(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input className="form-control" value={password} onChange={e => setPassword(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">E-mail</label>
+                        <input className="form-control" value={email} onChange={e => setEmail(e.target.value)}/>
+                    </div>
+                    {error && <div className="alert alert-danger">{error}</div>}
+                    <button className="btn btn-primary w-100" type="submit">Sign Up</button>
+                </form>
+                <hr/>
+                <button className="btn btn-outline-secondary w-100" onClick={() => navigate("/")}>Back to Login page</button>
+            </div>
         </div>
     );
 }
